76d38073fb68802b541cd8e15d3a81de5bb69f14
HitayshiShah <hitayshi27@gmail.com>
2021-04-24 16:46:31 -0700

    Trial commit from Hitayshi

---
+++ banking-app/src/edu/sjsu/Tester.java
@@ -0,0 +1,9 @@
+package edu.sjsu;
+
+public class Tester {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("Trying - Hitayshi");
+    }
+}
